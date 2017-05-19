e6a12a5591105330675fcf3597e421e2a6beabfa
Ladsond3 <ldubos8474@greenvilleschools.us>
2017-05-19 15:14:55 -0400

    Add files via upload

--- js/instrument_type_checker.js
+++ js/instrument_type_checker.js
@@ -7,6 +7,7 @@ function checkIt() {
   var clarinet = 0;
   var tuba = 0;
 
+
   // Get all checkbox elements from form
   var elements = document.getElementsByClassName('answer');
 
@@ -52,25 +53,43 @@ function checkIt() {
   var message;
 
   if (max == drum) {
-    message = "drum.";
+    message = "You got drums. You either joined because you're a musical prodigy or you've never been able to read music and watch 'Drumline' on repeat."
+
+    img = "<img src='img/drummarchingband.jpeg'>";
+
   }
   else if (max == trumpet) {
-    message = "trumpet.";
+    message = "You got trumpet. You spend your days popping blood vessels trying to hit the highest note only dogs can here. What an ego boost."
+
+    img = "<img src='img/marchingbandtrumpet.jpg'>";
   }
   else if (max == guard) {
-    message = "guard";
+    message = "You're in the color guard. Your flag may not be an instrument, but it is definitely one deadly weapon."
+
+    img = "<img src='img/marchingbandguard.jpg'>";
   }
   else if (max == sax) {
-    message = "saxophone.";
+    message = "You're a saxophone player! You're the life of the party and all the chicks dig you. Good for you."
+
+    img = "<img src='img/marchingband2.jpg'>";
   }
   else if (max == tuba) {
-    message = "tuba.";
+    message = "You got the tuba. No matter how many times you say it's a sousaphone we'll still call it a tuba."
+
+    img = "<img src='img/clarinet.jpg'>";
   }
   else if (max == clarinet) {
-    message = "clarinet";
+    message = "You play the clarinet. You spend hours locked in your room mastering your craft to get back at all the kids who compared you to squidward."
+
+    img = "<img src='img/tuba.jpg'>";
   }
 
+
+
+
+
   // Display result
-  document.getElementById('result-text').innerHTML = counts;
-  // document.getElementById('result-text').innerHTML = message;
+  // document.getElementById('result-text').innerHTML = counts;
+   document.getElementById('result-text').innerHTML = message;
+
 }
